4e549ef0fd539af8e017ba67944d1ddc81ac2a14
SuminJN <21900635@handong.edu>
2021-12-11 17:23:42 +0900

    DAO 작성완료

--- src/main/java/com/mycom/myapp/comment/CommentDAO.java
+++ src/main/java/com/mycom/myapp/comment/CommentDAO.java
@@ -1,5 +1,40 @@
 package com.mycom.myapp.comment;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class CommentDAO {
 
+	@Autowired
+	SqlSession sqlSession;
+	
+	public int insertComment(CommentVO vo) {
+		int result = sqlSession.insert("Comment.insertComment", vo);
+		return result;
+	}
+	
+	public int deleteComment(int id) {
+		int result = sqlSession.insert("Comment.deleteComment", id);
+		return result;
+	}
+	
+	public int updateComment(CommentVO vo) {
+		int result = sqlSession.insert("Comment.updateComment", vo);
+		return result;
+	}
+	
+	public CommentVO getComment(int seq) {
+		CommentVO one = sqlSession.selectOne("Comment.getComment", seq);
+		return one;
+	}
+	
+	public List<CommentVO> getCommentList(){
+		List<CommentVO> list = sqlSession.selectList("Comment.getCommentList");
+		return list;
+	}
+	
 }
--- src/main/java/com/mycom/myapp/todolist/TodoListDAO.java
+++ src/main/java/com/mycom/myapp/todolist/TodoListDAO.java
@@ -1,5 +1,40 @@
 package com.mycom.myapp.todolist;
 
-public class TodoListDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TodoListDAO {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	public int insertTodoList(TodoListVO vo) {
+		int result = sqlSession.insert("TodoList.insertTodoList", vo);
+		return result;
+	}
+	
+	public int deleteTodoList(int id) {
+		int result = sqlSession.insert("TodoList.deleteTodoList", id);
+		return result;
+	}
+	
+	public int updateTodoList(TodoListVO vo) {
+		int result = sqlSession.insert("TodoList.updateTodoList", vo);
+		return result;
+	}
+	
+	public TodoListVO getTodoList(int seq) {
+		TodoListVO one = sqlSession.selectOne("TodoList.getTodoList", seq);
+		return one;
+	}
+	
+	public List<TodoListVO> getTodoListList(){
+		List<TodoListVO> list = sqlSession.selectList("TodoList.getTodoListList");
+		return list;
+	}
+	
 }
